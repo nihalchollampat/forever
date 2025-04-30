7922c82c699ca952bd812070002f9cfb13be637a
nihalcghollampat <nihalchollampat@gmail.com>
2025-04-30 11:51:37 +0530

    updates

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -7,8 +7,8 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const currency = '$';
-    const delivery_fee = 10;
+    const currency = '₹';
+    const delivery_fee = 100;
     const navigate = useNavigate();
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
