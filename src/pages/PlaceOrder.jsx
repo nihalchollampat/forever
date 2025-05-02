d4e61eb1dd2b7eb169e12565d12a65186b6b8df0
nihalcghollampat <nihalchollampat@gmail.com>
2025-05-02 23:47:32 +0530

    paytm

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
+
 import { ShopContext } from '../context/ShopContext'
 import { useNavigate } from 'react-router-dom'
 
@@ -43,7 +44,7 @@ const PlaceOrder = () => {
 
                 <div className='mt-12'>
                     <Title text1={'PAYMENT'} text2={'METHOD'} />
-                    <div className='flex gap-3 flex-col lg:flex-row'>
+                    <div className='flex gap-3  lg:flex-row'>
                         <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
                             <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
@@ -55,6 +56,11 @@ const PlaceOrder = () => {
                         <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
                             <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
                             <p className=' text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+                        </div>
+                        <div onClick={() => setMethod('paytm')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                            <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'paytm' ? 'bg-green-400' : ''}`}></p>
+                            <img className='h-7 mx-4' src={assets.paytm_logo} alt="Paytm" />
+                          
                         </div>
                     </div>
                     <div className='w-full text-end mt-8'>
