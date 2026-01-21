317d3fdd91d946200a40be255fbc88b46842d953
nihalchollampat <nihalchollampat@gmail.com>
2026-01-21 19:24:53 +0530

    fixed the structure

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://demggjzwvfsxxlmxeryi.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlbWdnanp3dmZzeHhsbXhlcnlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxODQ4MDIsImV4cCI6MjA3ODc2MDgwMn0.9-DPemkhg34lGiey4RlbtChK60rMfEn1EKzI3jczc-8'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
